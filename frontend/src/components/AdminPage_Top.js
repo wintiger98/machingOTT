8a890f997d7411400721e8cab56f30c4d9cf63b7
SEO <devouring123@nave.com>
2021-11-06 19:40:14 +0900

    프로필 화면 수정중 1

--- frontend/src/components/AdminPage_Top.js
+++ frontend/src/components/AdminPage_Top.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import Customer from "./Customer";
-import {Table, TableBody, TableCell, TableRow, TableHead ,withStyles} from "@material-ui/core";
+import {Table, TableBody, TableCell, TableRow, TableHead, withStyles} from "@material-ui/core";
 
 const styles = theme => ({
     root: {
@@ -16,26 +16,44 @@ const styles = theme => ({
 // customer 배열
 const customerArray = [
     {
-        'id': 1,
-        'image': 'https://placeimg.com/150/150/1',
-        'name': '서명교',
-        'birthday': '980823',
+        'profileImage': 'https://placeimg.com/150/150/1',
+        'userName': '서시대생서명교',
+        'OTTName': 'NETFLIX',
+        'groupID': 1,
+        'userID': 'devouring123',
+        'userPassword': 'asdf1234',
+        'realName': '서명교',
+        'registrationNumber': '9808231234567',
+        'phoneNum': '01077434495',
+        'mailAddress': 'devouring123@gmail.com',
         'sex': 'Male',
         'job': 'Student',
     },
     {
-        'id': 2,
-        'image': 'https://placeimg.com/150/150/2',
-        'name': '김윤호',
-        'birthday': '980111',
+        'profileImage': 'https://placeimg.com/150/150/2',
+        'userName': '사실김윤호',
+        'OTTName': 'WATCHA',
+        'groupID': 2,
+        'userID': 'rladbsgh123',
+        'userPassword': 'zxcv1234',
+        'realName': '서명교',
+        'registrationNumber': '9801011234567',
+        'phoneNum': '01012345678',
+        'mailAddress': 'rladbsgh123@gmail.com',
         'sex': 'Female',
         'job': 'SalaryMan',
     },
     {
-        'id': 3,
-        'image': 'https://placeimg.com/150/150/3',
-        'name': '임유성',
-        'birthday': '980517',
+        'profileImage': 'https://placeimg.com/150/150/3',
+        'userName': '임유성123',
+        'OTTName': 'WAVVE',
+        'groupID': 3,
+        'userID': 'dbtjd112233',
+        'userPassword': 'qwer1234',
+        'realName': '서명교',
+        'registrationNumber': '9812311234567',
+        'phoneNum': '01087654321',
+        'mailAddress': 'dbtjd112233@gmail.com',
         'sex': 'Male',
         'job': 'None',
     }
@@ -43,24 +61,38 @@ const customerArray = [
 
 class AdminPage_Top extends Component {
     render() {
-        const {classes} = this.props;
         return (
             <div>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell>ID</TableCell>
-                            <TableCell>이미지</TableCell>
-                            <TableCell>이름</TableCell>
-                            <TableCell>생년월일</TableCell>
+                            <TableCell>프로필사진</TableCell>
+                            <TableCell>닉네임</TableCell>
+                            <TableCell>OTT 이름</TableCell>
+                            <TableCell>그룹 ID</TableCell>
+
+                            <TableCell>아이디</TableCell>
+                            <TableCell>비밀번호</TableCell>
+                            <TableCell>실명</TableCell>
+                            <TableCell>전화번호</TableCell>
+
+                            <TableCell>주민번호</TableCell>
+                            <TableCell>메일주소</TableCell>
                             <TableCell>성별</TableCell>
                             <TableCell>직업</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {customerArray.map(c => {
-                                return <Customer key={c.id} id={c.id} image={c.image} name={c.name}
-                                                 birthday={c.birthday} sex={c.sex} job={c.job}/>
+                            return <Customer key={c.id} profileImage={c.profileImage}
+                                             userName={c.userName} OTTName={c.OTTName}
+                                             groupID={c.groupID} userID={c.userID}
+                                             userPassword={c.userPassword} realName={c.realName}
+                                             phoneNum={c.phoneNum}
+                                             registrationNumber={c.registrationNumber}
+                                             mailAddress={c.mailAddress} sex={c.sex}
+                                             job={c.job}
+                            />
                         })}
                     </TableBody>
                 </Table>
