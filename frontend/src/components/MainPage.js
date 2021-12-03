b3b84395c96bedf38333b43e90dc5220fb555066
Heesun Park <phs1585@naver.com>
2021-12-03 23:27:13 +0900

    main btn

--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -4,17 +4,17 @@ import Header from "./Header";
 import surf from "./MainPage.jpg";
 
 export default function MainPage(props) {
-  const [isActive, setisActive] = useState(false);
+  const [isLogin, setisLogin] = useState(false);
   const [isAttend, setisAttend] = useState(false);
 
   console.log(props.location.state);
   useEffect(() => {
-    setisActive(props.location.state);
+    setisLogin(props.location.state);
   }, [props.location.state]);
 
   return (
     <div>
-      <Header LoginState={isActive} AttendState={isAttend} />
+      <Header LoginState={isLogin} AttendState={isAttend} />
       <div
         style={{
           display: "flex",
@@ -28,14 +28,41 @@ export default function MainPage(props) {
         <div className="app-wrapper">
           <img src={surf} className="MainPage" alt="MainPage" />
           <form className="form-wrapper">
-            <button className="submit" a href="/Signup">
-              시작하기
+            <button className="submit">
+              {" "}
+              <a
+                href="/login"
+                style={{
+                  fontFamily: "GmarketSansLight",
+                  fontSize: "20px",
+                  fontWeight: "bold",
+                  textDecoration: "none",
+                  color: "white",
+                }}
+              >
+                시작하기
+              </a>
             </button>
           </form>
         </div>
       </div>
     </div>
   );
+}
+
+{
+  /* <div>
+          <h1>UOMS</h1>
+          <button>
+            <Link
+              to="/login"
+              style={{ textDecoration: "none", marginRight: "5px" }}
+            >
+              로그인
+            </Link>
+          </button>
+          뒷배경에는 이미지 넣어보기
+        </div> */
 }
 
 {
